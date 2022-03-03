838118af4e8aa6e43778b3b88cfea8571be68f4c
Roberta Pennisi <roberta.pennisi97@gmail.com>
2022-03-03 11:53:48 +0100

    feat: added questions card

---
+++ components/QuestionsCard/index.jsx
@@ -0,0 +1,36 @@
+import styles from './QuestionsCard.module.scss'
+
+export default function QuestionsCard({
+    questionID = 0, 
+    question = 'Di che colore era il cavallo bianco di Napoleone?', 
+    answers = [
+        {
+            text: "570 a.C.",
+            correct: true
+        },
+{
+            text: "470 a.C.",
+            correct: false,
+        },
+{
+            text: "530 d.C",
+            correct: false,
+        },
+{
+            text: "530 a.C",
+            correct: false,
+        },
+    ],
+}){
+    return(
+        <div className={styles.QuestionsCard}>
+            <div className={styles.question}>
+                <h1>Domanda n° {questionID}/8</h1>
+                <p>{question}</p>
+            </div>
+            <div className={styles.answers}>
+                {answers.map(answer => <button>{answer.text}</button>)}
+            </div>
+        </div>
+    )
+}
