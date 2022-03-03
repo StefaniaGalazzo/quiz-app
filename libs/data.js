7057cf1bb5711e4610580d63c30ab4ced554a8cf
MarcoM94 <voldrak94@gmail.com>
2022-03-03 10:21:32 +0100

    feat: data

---
+++ libs/data.js
@@ -0,0 +1,178 @@
+const data = [
+    {
+        id: 1,
+        question: "In che anno nacque Pitagora?",
+            answers: [
+                {
+                text: "570 a.C.",
+                correct: true
+            },
+    {
+                text: "470 a.C.",
+                correct: false,
+            },
+    {
+                text: "530 d.C",
+                correct: false,
+            },
+    {
+                text: "530 a.C",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 2,
+        question: "Quanti abitanti ha Roma?",
+            answers: [
+                {
+                text: "3.144.543 circa",
+                correct: false,
+            },
+    {
+                text: "2.463.355 circa",
+                correct: false,
+            },
+    {
+                text: "2.763.804 circa",
+                correct: true,
+            },
+    {
+                text: "3.063.174 circa",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 3,
+        question: "Chi fu il vero inventore della lampadina?",
+            answers: [
+                {
+                text: "Io",
+                correct: false,
+            },
+    {
+                text: "Aimé Argand",
+                correct: false,
+            },
+    {
+                text: "Nikola Tesla",
+                correct: false,
+            },
+    {
+                text: "Thomas Edison",
+                correct: true,
+            },
+        ]
+    },
+    {
+        id: 4,
+        question: "Chi è l'attuale regina d'inghilterra?",
+            answers: [
+                {
+                text: "Elisabetta I",
+                correct: false,
+            },
+    {
+                text: "Elisabetta II",
+                correct: true,
+            },
+    {
+                text: "Elisabetta III",
+                correct: false,
+            },
+    {
+                text: "Paperina",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 5,
+        question: "",
+            answers: [
+                {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 6,
+        question: "",
+            answers: [
+                {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 7,
+        question: "",
+            answers: [
+                {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+    {
+                text: "",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 8,
+        question: "Chi è il team che ha creato questo quiz?",
+            answers: [
+                {
+                text: "team 1",
+                correct: false,
+            },
+    {
+                text: " team 2",
+                correct: false,
+            },
+    {
+                text: "team 3",
+                correct: true
+            },
+    {
+                text: "nessuno dei precedenti",
+                correct: false,
+            },
+        ]
+    },
+    ]
