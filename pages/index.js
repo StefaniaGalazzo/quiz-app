d18eb42c0c103fca5f57ac13d027e1f484f5cdb7
Stefania Galazzo <galazzostefania@gmail.com>
2022-03-03 11:51:54 +0100

    feat:add answers recap comp

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,11 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        
-      </main>
-
-     
+      <main className={styles.main}></main>
     </div>
-  )
+  );
 }
